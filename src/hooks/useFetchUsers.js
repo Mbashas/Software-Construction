19c600cd28c95c34e7305c32308c7c7be9846a35
sethmbasha <sethmbasha@gmail.com>
2025-02-11 11:35:24 +0300

    fix: correct import statement in useFetchUsers hook

--- src/hooks/useFetchUsers.js
+++ src/hooks/useFetchUsers.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { fetchUsers } from "../services/userService";
+import { fetchUsers ,} from "../services/userService";
 
 export default function useFetchUsers() {
   const [users, setUsers] = useState([]);
