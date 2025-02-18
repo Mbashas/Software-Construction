9752ddc334c22f3801c37500c3061e4a95f75e52
sethmbasha <sethmbasha@gmail.com>
2025-02-18 19:56:38 +0300

    feat: implement custom user model and task assignment functionality

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    groups = models.ManyToManyField(
+        Group,
+        blank=True,
+        related_name="customuser_groups"
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        blank=True,
+        related_name="customuser_permissions"
+    )
