e33fbb666b303451e314586cf667a0e0bdfe9917
sethmbasha <sethmbasha@gmail.com>
2025-02-19 16:19:20 +0300

    feat: add URL routing and view for user list creation

--- backend/users/views.py
+++ backend/users/views.py
@@ -3,7 +3,6 @@ from rest_framework import generics
 from .models import CustomUser
 from .serializers import UserSerializer
 
-# Provides both GET (list) and POST (create) for users
-class UserListCreate(generics.ListCreateAPIView):
+class UserListCreateView(generics.ListCreateAPIView):  # Make sure this name matches exactly
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
